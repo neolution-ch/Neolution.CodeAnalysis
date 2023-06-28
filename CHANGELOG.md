ab0a2bdd2d4ec2d4a9e8a66cc24ec4bff0b64926
neoscie <sc@neolution.ch>
2023-06-28 13:43:23 +0200

    Neolution CodeAnalysis V3 (#14)
    * Update README.md
    * Add changelog
    * Fix CI and CD
    * Update rules
    * Add previously added SonarSource rules
    * Add rules from Microsoft .NET Minimum Ruleset
    * Group same rules together for a better overview
    * Fix Tests ruleset
    ---------
    
    Co-authored-by: GitHub Release Bot <release-bot@neolution.ch>
    Co-authored-by: neotrow <92933708+neotrow@users.noreply.github.com>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,17 @@ and adheres to a project-specific [Versioning](/README.md).
 
 ## [Unreleased]
 
+### Changed
+
+- Stopped changing all rules to error first and instead use the default rulesets of the Analyzers as the base ru
+- Migrate from \*.ruleset files to \*.globalconfig files
+- Migrate from \*.targets to \*. props files
+- Simplified and decluttered README file
+
+### Removed
+
+- Removed FxCop Analyzer because it is now bundled with .NET SDK
+
 ## [2.7.1] - 2023-06-14
 
 ### Added
---
+++ NugetPack.ps1
@@ -0,0 +1,27 @@
+$nugetPath = ".\NuGet.exe"
+
+if (-not (Test-Path $nugetPath)) {
+    Write-Output "NuGet executable not found. Downloading..."
+    Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile $nugetPath
+}
+
+$date = Get-Date -format "yyyy.M.d"
+$time = Get-Date -format "HHmmss"
+$versionString = $date + "-dev" + $time
+Write-Output "Version: $versionString"
+
+Get-ChildItem -Include *.nuspec, *.props -Recurse | ForEach-Object {
+    (Get-Content $_.FullName) -replace '{{NuGetVersion}}', $versionString |
+        Set-Content $_.FullName
+}
+
+.\NuGet.exe pack Neolution.CodeAnalysis.nuspec -OutputDirectory .\bin\packages
+.\NuGet.exe pack Neolution.CodeAnalysis.TestsRuleset.nuspec -OutputDirectory .\bin\packages
+
+Get-ChildItem -Include *.nuspec, *.props -Recurse | ForEach-Object {
+    (Get-Content $_.FullName) -replace $versionString, '{{NuGetVersion}}' |
+        Set-Content $_.FullName
+}
+
+# Wait for user input before closing the window
+Read-Host -Prompt "Press Enter to exit"
